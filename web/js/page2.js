9bbff69312006f6c247ee7405ba68e1c530500d8
luca <luca.gugole99@gmail.com>
2018-06-27 23:15:53 +0200

    edited web gui and added tests

--- web/js/page2.js
+++ web/js/page2.js
@@ -2,48 +2,73 @@ $(document).ready(function ($) {
 
     var vulnInfo = {};
 
-    function generatePieChart() {
+    $("#vulnscan_button").on("click", function () {
+        generatePieChart();
+        // $.ajax({
+        //     url : 'http://localhost:8080/exec',
+        //     type : 'GET',
+        //     dataType: "json",
+        //     data : {
+        //         command: "",
+        //         parameters: "",
+        //         launch_time: ""
+        //     },
+        //     success: function (e) {
+        //         console.log(e);
+        //         vulnInfo = e;
+        //         generatePieChart();
+        //     }
+        // });
+    });
 
+    function generatePieChart() {
+        var canvas = $("#vuln_chart");
+
+        var options = {
+            title: {
+                display: true,
+                text: "Vulnerabilities risk levels"
+            },
+            legend: {
+                display: false,
+                position: 'bottom'
+            },
+            animation: {
+                duration: 2000
+            },
+            layout: {
+                padding: {
+                    left: 0,
+                    right: 0,
+                    top: 0,
+                    bottom: 0
+                }
+            }
+        };
+
+        var data = {
+            datasets: [{
+                data: [10, 20, 30, 70],
+                backgroundColor: ['red', 'orange', 'yellow', 'blue']
+            }],
+
+            labels: [
+                'High',
+                'Medium',
+                'Low',
+                'Log'
+            ]
+        };
+
+        var chart = new Chart(canvas, {
+            type: 'pie',
+            data: data,
+            options: options
+        });
     }
 
     function generateVulnTable() {
-        //delete old table, if exists
-        //destroyTable();
 
-        table = document.getElementById("table_div");
-
-        //header row
-        var header = table.createTHead();
-        var iRow = header.insertRow(0);
-        var iCell1 = iRow.insertCell(0);
-        var iCell2 = iRow.insertCell(1);
-        var iCell3 = iRow.insertCell(2);
-        var iCell4 = iRow.insertCell(3);
-        iCell1.innerHTML = "Status".bold();
-        iCell2.innerHTML = "Date".bold();
-        iCell3.innerHTML = "Value in open".bold();
-        iCell4.innerHTML = "Value in close".bold();
-        iCell1.style.background = "#D2D2D2";
-        iCell2.style.background = "#D2D2D2";
-        iCell3.style.background = "#D2D2D2";
-        iCell4.style.background = "#D2D2D2";
-
-
-        //data rows
-        for (var i=0; i<listDate.length; i++) {
-            date = listDate[i];
-            var row = table.insertRow(i+1);
-            if (dataOutput['Time Series (Daily)'][date]['1. open'] < dataOutput['Time Series (Daily)'][date]['4. close']) {
-                row.insertCell(0).innerHTML = "<img src=img/Increase.svg width=15 height=15>";
-            } else if (dataOutput['Time Series (Daily)'][date]['1. open'] > dataOutput['Time Series (Daily)'][date]['4. close']){
-                row.insertCell(0).innerHTML = "<img src=img/Decrease.png width=15 height=15>";
-            } else {
-                row.insertCell(0).innerHTML = "<img src=img/Steady.png width=15 height=15>";
-            }
-            row.insertCell(1).innerHTML = date;
-            row.insertCell(2).innerHTML = dataOutput['Time Series (Daily)'][date]['1. open'];
-            row.insertCell(3).innerHTML = dataOutput['Time Series (Daily)'][date]['4. close'];
-        }
     }
 
     function destroyPieChart() {
--- web/js/page3.js
+++ web/js/page3.js
@@ -1,3 +1,44 @@
 $(document).ready(function ($) {
 
+    var exploitInfo = {};
+
+    $("#exploit_button").on("click", function () {
+        var parameters = function () {
+            var result = "-q -x"
+        };
+        $.ajax({
+            url : 'http://localhost:8080/exec',
+            type : 'GET',
+            dataType: "json",
+            data : {
+                command: "msfconsole",
+                parameters: "",
+                launch_time: ""
+            },
+            beforeSend: function() {
+                changeResultTextStatus("trying to access...", "#0066ff");
+            },
+            success: function (e) {
+                console.log(e);
+                exploitInfo = e;
+                changeResultTextStatus("success: access granted", "#009900");
+            },
+            error: function (e) {
+                console.log(e);
+                changeResultTextStatus("error: access not granted", "#ff0000")
+            }
+        });
+    });
+
+
+    function changeResultTextStatus(newValue, color) {
+        var element = $("#resultText");
+        element.html(newValue);
+        element.css("color", color);
+    }
+
+    function generateTable() {
+
+    }
+
 });
