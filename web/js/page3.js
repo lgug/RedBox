0288e891d239b1afb12c22742ab23e62bc394206
luca <luca.gugole99@gmail.com>
2018-06-28 21:37:35 +0200

    edited web gui and created adapter for metasploit

--- web/js/page3.js
+++ web/js/page3.js
@@ -2,6 +2,44 @@ $(document).ready(function ($) {
 
     var exploitInfo = {};
 
+    $("#exp_search").on("click", function () {
+        var osFilter = $("#exp_os");
+        var nameFilter = $("#exp_name");
+        var parameters = function () {
+            var result = "-q__-x__search type:exploit";
+            if (osFilter.val() !== null)  {
+                result += " platform:" + osFilter.val();
+            }
+            if (nameFilter.val() !== null) {
+                result += " name:" + nameFilter.val();
+            }
+            result += ";exit";
+            return result;
+        };
+        $.ajax({
+            url : 'http://localhost:8080/exec',
+            type : 'GET',
+            dataType: "json",
+            data : {
+                command: "msfconsole",
+                parameters: parameters()
+            },
+            beforeSend: function() {
+                changeResultTextStatus("trying to access...", "#0066ff");
+            },
+            success: function (e) {
+                console.log(e);
+                exploitInfo = e;
+                generateTable();
+                changeResultTextStatus("success: access granted", "#009900");
+            },
+            error: function (e) {
+                console.log(e);
+                changeResultTextStatus("error: access not granted", "#ff0000")
+            }
+        });
+    });
+
     $("#exploit_button").on("click", function () {
         var parameters = function () {
             var result = "-q -x"
@@ -29,7 +67,6 @@ $(document).ready(function ($) {
             }
         });
     });
-
 
     function changeResultTextStatus(newValue, color) {
         var element = $("#resultText");
